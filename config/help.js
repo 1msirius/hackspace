64c0f317c94ca5c02fe1624af9df81be9bf57b02
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2024-10-02 10:14:43 +0530

    first commit

---
+++ config/help.js
@@ -0,0 +1,15 @@
+const help = {
+  "%help%": "list all commands (you're looking at it)",
+  "%commit%": "learn about Commit",
+  "%apply%": "apply to Commit",
+  "%hackathon%": "list all hackathon projects",
+  "%hackathon% [project]": "learn about a hackathon project",
+  "%whois%": "list all partners",
+  "%whois% [partner]": "learn about a partner",
+  "%whois% conviction": "learn about us",
+  "%email%": "reach out to us",
+  "%twitter%": "twitter accounts",
+  "%locate%": "physical address",
+  "%sponsors%": "list all sponsors",
+  "%test%": "do not use",
+};
